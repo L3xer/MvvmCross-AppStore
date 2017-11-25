0ad3c2e53939f2ac0e44c88715bc056c3df2f090
Sanya <sanya2371@gmail.com>
2017-11-25 10:34:47 +0200

    Add category cell.

---
+++ AppStore.iOS/Cells/CategoryCell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UIKit;
+
+namespace AppStore.iOS.Cells
+{
+    public class CategoryCell : UICollectionViewCell
+    {
+        public CategoryCell(IntPtr handle) : base(handle)
+        {
+            SetupViews();
+        }
+
+        private void SetupViews()
+        {
+            BackgroundColor = UIColor.Black;
+        }
+    }
+}
--- AppStore.iOS/Views/MainView.cs
+++ AppStore.iOS/Views/MainView.cs
@@ -1,13 +1,17 @@
-﻿using MvvmCross.iOS.Views;
-using Appstore.Core.ViewModels;
+﻿using System;
 using UIKit;
 using Foundation;
-using System;
+using CoreGraphics;
+using MvvmCross.iOS.Views;
+using Appstore.Core.ViewModels;
+using AppStore.iOS.Cells;
 
 namespace AppStore.iOS.Views
 {
-    public class MainView : MvxCollectionViewController<MainViewModel>
+    public class MainView : MvxCollectionViewController<MainViewModel>, IUICollectionViewDelegateFlowLayout
     {
+        private readonly string cellId = "CategoryCellId";
+
         public MainView() : base(new UICollectionViewFlowLayout())
         {
 
@@ -18,6 +22,27 @@ namespace AppStore.iOS.Views
             base.ViewDidLoad();
 
             CollectionView.BackgroundColor = UIColor.White;
+            CollectionView.RegisterClassForCell(typeof(CategoryCell), cellId);
         }
+
+        public override UICollectionViewCell GetCell(UICollectionView collectionView, NSIndexPath indexPath)
+        {
+            return collectionView.DequeueReusableCell(cellId, indexPath) as CategoryCell;
+        }
+
+        public override nint GetItemsCount(UICollectionView collectionView, nint section)
+        {
+            return 3;
+        }
+
+        #region IUICollectionViewDelegateFlowLayout
+
+        [Export("collectionView:layout:sizeForItemAtIndexPath:")]
+        public CGSize GetSizeForItem(UICollectionView collectionView, UICollectionViewLayout layout, NSIndexPath indexPath)
+        {
+            return new CGSize(View.Frame.Width, 150);
+        }
+
+        #endregion
     }
 }
