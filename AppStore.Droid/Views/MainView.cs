addcb3f22a025f59b6247c210d01bd557e402b4b
Sanya <sanya2371@gmail.com>
2017-12-04 19:36:09 +0200

    Add android project.

---
+++ AppStore.Droid/Views/MainView.cs
@@ -0,0 +1,19 @@
+﻿using Android.OS;
+using Android.App;
+using MvvmCross.Droid.Views;
+using Appstore.Core.ViewModels;
+
+
+namespace AppStore.Droid.Views
+{
+    [Activity(Label = "Featured Apps", MainLauncher = true)]
+    public class MainView : MvxActivity<MainViewModel>
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.MainView);
+        }
+    }
+}
